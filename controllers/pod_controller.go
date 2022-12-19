66524ec17d8002b296ac797586a7453dc3cac01b
Amitai Getzler <amitai.getzler@explorium.ai>
2022-12-19 16:03:06 +0200

    :ambulance: fix min
    modified:   controllers/pod_controller.go

--- controllers/pod_controller.go
+++ controllers/pod_controller.go
@@ -117,14 +117,14 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 								sumCAddr.MinCPU = int64(math.Min(float64(sumCAddr.MinCPU), float64(latestC.CPU)))
 							} else {
 								sumCAddr.MaxCPU = latestC.CPU
-								sumCAddr.MinCPU = latestC.CPU
+								sumCAddr.MinCPU = 1
 							}
 							if latestC.Memory > 0 {
 								sumCAddr.MaxMemory = int64(math.Max(float64(sumCAddr.MaxMemory), float64(latestC.Memory)))
 								sumCAddr.MinMemory = int64(math.Min(float64(sumCAddr.MinMemory), float64(latestC.Memory)))
 							} else {
 								sumCAddr.MaxMemory = latestC.Memory
-								sumCAddr.MinMemory = latestC.Memory
+								sumCAddr.MinMemory = 1
 							}
 							sumCAddr.CPU += latestC.CPU
 							sumCAddr.Memory += latestC.Memory
